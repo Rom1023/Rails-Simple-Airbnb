b63307304aa93bc22a069ca9d92f5ccea920973f
romuald gaboriau <gaboriau.r@gmail.com>
2021-02-17 16:47:29 -0500

    Cards containing flat infos created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Bunker, patio,spa ON TOP parking intérieur dispo',
+  address: '123 rue Ste Catherine Montreal',
+  description: 'Mini studio-loft avec tout ce que vous avez de besoin pour un séjour dans la métropole tant pour dormir que pour cuisiner ( pas de fourneau) sur place',
+  price_per_night: 125,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'GOCLANDS',
+  address: '7546 rue Gasgrain Paris',
+  description: 'Chambre privée dans chambres d’hôtes – hôte : Phénix',
+  price_per_night: 135,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Idéalement situé et tout confort / RER+Métros',
+  address: '473 avenue Generale de Gaulle Paris',
+  description: 'Logement entier – hôte : Lucie',
+  price_per_night: 105,
+  number_of_guests: 2
+)
